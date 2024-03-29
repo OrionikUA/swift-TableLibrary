48d91b7130a4f2af3ba1cc9c4d2edb130beec829
Ihor Popkov <ihor.popkov@gmail.com>
2024-03-29 20:18:06 +0200

    TableDefaults Public

--- Sources/TableDefaults.swift
+++ Sources/TableDefaults.swift
@@ -3,17 +3,17 @@ import SwiftUI
 
 @available(macOS 14, *)
 public struct TableDefaults {
-    let color: Color
-    let width: CGFloat?
-    let hoverColor: Color?
-    let handHover: Bool
-    let clickColor: Color
-    let columnContentColor: Color
-    let columnContent: String
-    let columnContentSpace: CGFloat
-    let titleTextColor: Color
-    let titleBackColor: Color
-    let titleAlighnment: HorizontalAlignment
+    public let color: Color
+    public let width: CGFloat?
+    public let hoverColor: Color?
+    public let handHover: Bool
+    public let clickColor: Color
+    public let columnContentColor: Color
+    public let columnContent: String
+    public let columnContentSpace: CGFloat
+    public let titleTextColor: Color
+    public let titleBackColor: Color
+    public let titleAlighnment: HorizontalAlignment
     
     public init(color: Color, width: CGFloat?, hoverColor: Color?, handHover: Bool, clickColor: Color, columnContentColor: Color, columnContent: String, columnContentSpace: CGFloat, titleTextColor: Color, titleBackColor: Color, titleAlighnment: HorizontalAlignment) {
         self.color = color
